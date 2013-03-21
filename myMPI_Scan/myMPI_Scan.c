3250f94c1cf6716391abd91b087be8969214f583
David Xu <mysolowing@gmail.com>
2013-03-21 15:03:50 -0500

    running mpi_scan

--- myMPI_Scan/myMPI_Scan.c
+++ myMPI_Scan/myMPI_Scan.c
@@ -13,9 +13,19 @@ void Init(int argc, char *argv[], params_t *par);
 void myMPI_Scan();
 void cleanup(params_t *par);
 
-int SameOutput(int* a, int* b, size_t num);
+int SameOutput(int* a, int* b, int num,params_t *par);
 
 void PrintArray(params_t *par);
+void PrintArrayC(params_t *par);
+void PrintArrayA(params_t *par);
+
+void InsertNewLine(params_t *par){
+	MPI_Barrier(MPI_COMM_WORLD );
+	if (par->myid==0){
+		printf("\n");
+	}
+	MPI_Barrier(MPI_COMM_WORLD );
+}
 
 int main(int argc, char *argv[]) {
 	params_t par;
@@ -34,43 +44,87 @@ int main(int argc, char *argv[]) {
 
 	MPI_Barrier(MPI_COMM_WORLD );
 	Tstart(par.timer_1);
-	MPI_Scan(par.a, par.a, par.n, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+	MPI_Scan(par.c, par.a, par.n, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
 	Tstop(par.timer_1);
 	MPI_Barrier(MPI_COMM_WORLD );
 
-	PrintArray(&par);
 	MPI_Barrier(MPI_COMM_WORLD );
+	Tstart(par.timer_2);
+//	MPI_Scan(par.c, par.b, par.n, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+	Tstop(par.timer_2);
+	MPI_Barrier(MPI_COMM_WORLD );
+
+	PrintArray(&par);
 	// Last call to MPI (REQUIRED)
+
+	SameOutput(par.a,par.b,par.n,&par);
+
+	MPI_Barrier(MPI_COMM_WORLD );
+	if (par.myid==par.num_procs-1){
+
+		printf("\n  OMP_SCAN timer1 (sec): \t %.7lf",par.timer_1);
+		printf("\nmyOMP_SCAN timer2 (sec): \t %.7lf",par.timer_2);
+	}
+	MPI_Barrier(MPI_COMM_WORLD );
+
 	MPI_Finalize();
 	return 0;
 }
 
 void PrintArray(params_t *par) {
-//	MPI_Win win;
 //	MPI_Win_lock(MPI_LOCK_EXCLUSIVE, par->myid, 0, win);
-	printf("\n");
-	printf("Proc %d/%d,%6s A ", par->myid, par->num_procs,
-			par->proc_name);
+
+	int prc_num;
+
+	for (prc_num=0;prc_num<par->num_procs;++prc_num){
+		MPI_Barrier(MPI_COMM_WORLD );
+		if (par->myid==prc_num){
+			printf("\n");
+			printf("Proc %d/%d,%6s A ", par->myid, par->num_procs,
+					par->proc_name);
+			int k;
+			for (k = 0; k < par->n; ++k) {
+				printf("%3d ", par->a[k]);
+			}
+			printf("\n");
+			printf("Proc %d/%d,%6s B ", par->myid, par->num_procs,
+					par->proc_name);
+			for (k = 0; k < par->n; ++k) {
+				printf("%3d ", par->b[k]);
+			}
+			printf("\n");
+			printf("Proc %d/%d,%6s C ", par->myid, par->num_procs,
+					par->proc_name);
+			for (k = 0; k < par->n; ++k) {
+				printf("%3d ", par->c[k]);
+			}
+			printf("\n");
+		}
+		MPI_Barrier(MPI_COMM_WORLD );
+
+	}
+
+
+//	MPI_Win_unlock(par->myid,win);
+}
+
+void PrintArrayC(params_t *par) {
 	int k;
-	for (k = 0; k < par->n; ++k) {
-		printf("%3d ", par->a[k]);
-	}
-	printf("\n");
-
-	printf("Proc %d/%d,%6s B ", par->myid, par->num_procs,
-			par->proc_name);
-	for (k = 0; k < par->n; ++k) {
-		printf("%3d ", par->b[k]);
-	}
-	printf("\n");
-
 	printf("Proc %d/%d,%6s C ", par->myid, par->num_procs,
 			par->proc_name);
 	for (k = 0; k < par->n; ++k) {
 		printf("%3d ", par->c[k]);
 	}
 	printf("\n");
-//	MPI_Win_unlock(par->myid,win);
+}
+void PrintArrayA(params_t *par) {
+	int k;
+	printf("Proc %d/%d,%6s A ", par->myid, par->num_procs,
+			par->proc_name);
+	for (k = 0; k < par->n; ++k) {
+		printf("%3d ", par->a[k]);
+	}
+	printf("\n");
 }
 
 void myMPI_Scan() {
@@ -81,24 +135,33 @@ void Init(int argc, char *argv[], params_t *par) {
 	par->timer_global = par->timer_1 = par->timer_2 = par->timer_3 =
 			par->timer_4 = 0;
 	par->n = strtol(argv[1], NULL, 10);
-	par->nlevels = (int) ceil(log(par->n) / M_LN2);
-	par->nalloc = (int) (pow(2, par->nlevels));
+	par->nlevels = (int) ceil(log(par->num_procs) / M_LN2);
+	par->nalloc = par->n ;
 
-	par->c = (int*) calloc((1 + par->nalloc), sizeof(int));
-	par->a = (int*) calloc((1 + par->nalloc), sizeof(int));
-	par->b = (int*) calloc((1 + par->nalloc), sizeof(int));
+	par->c = (int*) calloc(( par->nalloc), sizeof(int));
+	par->a = (int*) calloc(( par->nalloc), sizeof(int));
+	par->b = (int*) calloc(( par->nalloc), sizeof(int));
 	int k;
-	srand(time(NULL ));
+	srand(time(NULL)+par->myid);
 	for (k = 0; k < par->n; ++k) {
 		par->c[k] = (rand() % RAND_RANGE)+1;
-		par->b[k] = par->c[k];
-		par->a[k] = par->c[k];
 	}
 
 }
 
-int SameOutput(int* a, int* b, size_t num) {
-	return 1;
+int SameOutput(int* a, int* b, int num,params_t *par){
+	if (par->myid!=par->num_procs-1)
+		return -2;
+	if (par->myid==par->num_procs-1){
+		int k;
+		for (k=0;k<num;++k){
+			if (a[k]!=b[k])
+				printf("a[%d]=%d\nb[%d]=%d\n",k,a[k],k,b[k]);
+				return k;
+		}
+	}
+	printf("Same output for A and B\n");
+	return -1;
 }
 
 void cleanup(params_t *par) {
